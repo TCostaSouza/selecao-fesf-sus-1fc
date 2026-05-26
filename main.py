fdb1801869478856bfa4004c9dc8aff5979f8095
Tainan <tcostasouza0@gmail.com>
2026-05-26 14:34:08 -0300

    feat: Initiate app, add middleware and define basic call structure

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+# Inicializar FastAPI
+app = FastAPI();
+
+# Middleware para comunicação com sites externos.
+# ATENÇÃO!: Não reproduzir fora de ambiente local!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Permite acesso de qualquer origem (ideal para desenvolvimento local)
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos os métodos (GET, POST, DELETE, etc.)
+    allow_headers=["*"],
+)
+
+tarefas = [] 
+contador_id = 1
+
+class Tarefa(BaseModel):
+    texto: str
+
+    
