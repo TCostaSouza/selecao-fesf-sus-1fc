a322c0ded3f33c3e88a452fdc8edfc99a70ae326
Tainan <tcostasouza0@gmail.com>
2026-05-26 15:26:15 -0300

    feat: Add basic necessary states and buscar_tarefa to the DB

--- frontend-todo/app/page.tsx
+++ frontend-todo/app/page.tsx
@@ -1,5 +1,25 @@
 "use cliente"
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  
+  const [tarefas, setTarefas] = useState([]); 
+  const [novaTarefa, setNovaTarefa] = useState("");
+
+  const backendUrl = "http://127.0.0.1:8000/tarefas";
+
+  // Buscar tarefas
+  const buscarTarefas = async () => {
+    try {
+      const resposta = await fetch(backendUrl);
+      const dados = await resposta.json();
+      setTarefas(dados); // Salva as tarefas recebidas no estado
+    } catch (erro) {
+      console.error("Erro ao buscar tarefas:", erro);
+    }
+  };
+
+  useEffect(() => {
+    buscarTarefas();
+  }, []);
 }
